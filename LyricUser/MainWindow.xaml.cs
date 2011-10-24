24e06b997f1658472644a416af6027cc54326137
James Luckraft <lucky_james@hotmail.com>
2011-10-24 22:32:24 +0100

    Added F11 key binding

--- LyricUser/MainWindow.xaml.cs
+++ LyricUser/MainWindow.xaml.cs
@@ -23,5 +23,31 @@ namespace LyricUser
         {
             InitializeComponent();
         }
+
+        private void ToggleMaxmised()
+        {
+            switch (this.WindowState)
+            {
+                case WindowState.Maximized:
+                    this.WindowState = System.Windows.WindowState.Normal;
+                    break;
+                case WindowState.Minimized:
+                    System.Diagnostics.Debug.Print("Window is minimised, leave state unchanged.");
+                    break;
+                case WindowState.Normal:
+                    this.WindowState = System.Windows.WindowState.Maximized;
+                    break;
+                default:
+                    throw new ApplicationException("Unrecognised window state: " + this.WindowState);
+            }
+        }
+
+        private void Window_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (Key.F11 == e.Key)
+            {
+                ToggleMaxmised();
+            }
+        }
     }
 }
