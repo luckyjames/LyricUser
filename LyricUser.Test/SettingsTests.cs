354719195f92167bd47dc93d2c7436d9c3f354eb
James Luckraft <lucky_james@hotmail.com>
2011-10-31 23:36:01 +0000

    + Added test assembly with failing test + Added nunit project to run tests on release

---
+++ LyricUser.Test/SettingsTests.cs
@@ -0,0 +1,15 @@
+﻿
+using NUnit.Framework;
+
+namespace LyricUser.Test
+{
+    [TestFixture]
+    public class SettingsTests
+    {
+        [TestCase]
+        public void Failer()
+        {
+            Assert.Fail("First test fails");
+        }
+    }
+}
