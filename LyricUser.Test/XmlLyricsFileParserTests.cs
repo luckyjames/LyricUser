e5d21a1333ab86a3c1e2383b5ecc86dc6343d79b
James Luckraft <lucky_james@hotmail.com>
2011-11-01 00:12:30 +0000

    + Added first iteration of simple XMl parser + Improved nunit project to use VS project

---
+++ LyricUser.Test/XmlLyricsFileParserTests.cs
@@ -0,0 +1,24 @@
+﻿
+using NUnit.Framework;
+using System;
+
+namespace LyricUser.Test
+{
+    [TestFixture]
+    public class SettingsTests
+    {
+        [TestCase]
+        public void NullFails()
+        {
+            Assert.Catch<ArgumentNullException>( delegate { new XmlLyricsFileParser(null); } );
+        }
+
+        [TestCase]
+        public void SimpleTest()
+        {
+            const string relativePathToTestData = @"..\..\..\TestData\SimpleSong.xml";
+            XmlLyricsFileParser xmlLyricsFileParser = new XmlLyricsFileParser(relativePathToTestData);
+            Assert.IsNotNullOrEmpty(xmlLyricsFileParser.Lyrics);
+        }
+    }
+}
---
+++ LyricUser/XmlLyricsFileParser.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+
+namespace LyricUser
+{
+    /// <summary>
+    /// Parses some lyrics from a file
+    /// </summary>
+    public class XmlLyricsFileParser
+    {
+        private readonly string lyrics;
+        public string Lyrics
+        {
+            get
+            {
+                return lyrics;
+            }
+        }
+
+        public XmlLyricsFileParser(string xmlFileUrl)
+        {
+            const string lyricsElementName = "lyrics";
+            using (XmlTextReader xmlTextReader = new XmlTextReader(xmlFileUrl))
+            {
+                if (!xmlTextReader.ReadToDescendant(lyricsElementName))
+                {
+                    throw new ApplicationException("couldn't find element - " + lyricsElementName);
+                }
+                else
+                {
+                    this.lyrics = xmlTextReader.ReadElementContentAsString();
+                }
+            }
+        }
+    }
+}
