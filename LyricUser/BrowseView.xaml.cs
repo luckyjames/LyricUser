065c7735613effa3631b6c6a6e45c839bc878042
James <lucky_james@hotmail.com>
2012-05-06 12:13:30 +0100

    Generating installer

--- LyricUser/BrowseView.xaml.cs
+++ LyricUser/BrowseView.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel;
 using System.IO;
 using System.Windows;
 using System.Windows.Controls;
@@ -6,6 +7,51 @@ using System.Windows.Input;
 
 namespace LyricUser
 {
+    class LyricsFileSystemNode
+    {
+        private string nodePath;
+        public string NodePath
+        {
+            get
+            {
+                return nodePath;
+            }
+        }
+
+        public LyricsFileSystemNode(string nodePath)
+        {
+            this.nodePath = nodePath;
+        }
+    }
+
+    class LyricsFileSystem : FileSystemWatcher
+    {
+        private readonly LyricsFileSystemNode rootNode;
+
+        public LyricsFileSystem(string rootPath)
+        {
+            this.rootNode = new LyricsFileSystemNode(rootPath);
+            this.IncludeSubdirectories = true;
+            this.Filter = "*.xml";
+            this.Changed += new FileSystemEventHandler(LyricsFileSystem_Changed);
+            this.Created += new FileSystemEventHandler(LyricsFileSystem_Changed);
+            this.Deleted += new FileSystemEventHandler(LyricsFileSystem_Changed);
+            this.Renamed += new RenamedEventHandler(LyricsFileSystem_Renamed);
+        }
+
+        void LyricsFileSystem_Renamed(object sender, RenamedEventArgs e)
+        {
+            throw new NotImplementedException(
+                e.ChangeType.ToString() + " :" + e.OldFullPath + " -> " + e.FullPath);
+        }
+
+        void LyricsFileSystem_Changed(object sender, FileSystemEventArgs e)
+        {
+            throw new NotImplementedException(
+                e.ChangeType.ToString() + " :" + e.FullPath);
+        }
+    }
+
     /// <summary>
     /// Interaction logic for BrowseView.xaml
     /// </summary>
