38f1d35efbb97dae5fe5f5c65b8c6840f16ce390
James <lucky_james@hotmail.com>
2012-06-09 15:34:34 +0100

    Revert files spuriously removed during "Add unit test run as post-build step"
    This reverts commit 709bff3c9f0e77711a30fd9b85243757e20a7f02.

---
+++ LyricUser/App.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System.Windows;
+using System;
+
+namespace LyricUser
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private string lyricsUrl;
+        public string LyricsUrl
+        {
+            get
+            {
+                return lyricsUrl;
+            }
+            set
+            {
+                lyricsUrl = value;
+            }
+        }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            if (e.Args.Length < 1)
+            {
+                System.Diagnostics.Debug.WriteLine("No arguments available.");
+            }
+            else
+            {
+                lyricsUrl = e.Args[0];
+            }
+        }
+
+        private void LyricsViewerDispatcherUnhandledException(
+            object sender,
+            System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            Exception theException = e.Exception;
+            string traceFileName = "GeneratorTestbedError.txt";
+            string allUsersAppDataFolder = Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData);
+            if (string.IsNullOrEmpty(allUsersAppDataFolder))
+            {
+                allUsersAppDataFolder = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            }
+            string theErrorPath = System.IO.Path.Combine(allUsersAppDataFolder, traceFileName);
+            using (System.IO.TextWriter theTextWriter = new System.IO.StreamWriter(theErrorPath, true))
+            {
+                DateTime theNow = DateTime.Now;
+                theTextWriter.WriteLine("The error time: " + theNow.ToShortDateString() + " " + theNow.ToShortTimeString());
+                while (theException != null)
+                {
+                    theTextWriter.WriteLine("Exception: " + theException.ToString());
+                    theException = theException.InnerException;
+                }
+            }
+            MessageBox.Show("The program crashed. A stack trace can be found at:\n" + theErrorPath + "\n\nClosing the application..");
+            e.Handled = true;
+            Application.Current.Shutdown();
+        }
+    }
+}
